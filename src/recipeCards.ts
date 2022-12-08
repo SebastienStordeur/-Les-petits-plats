977cb69d640f6691cfecf393fef7e8aad30165a2
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-12-08 21:00:12 +0100

    Ability to filter using the input value + create arrays of each category for tag filtering

---
+++ src/recipeCards.ts
@@ -0,0 +1,87 @@
+interface recipe {
+  id: number;
+  name: string;
+  servings: number;
+  ingredients: {
+    ingredient: string;
+    quantity?: number;
+    unit?: undefined | string | number;
+  }[];
+  time: number;
+  description: string;
+  appliance: string;
+  ustensils: string[];
+}
+
+class Recipe {
+  id: number;
+  name: string;
+  description: string;
+  time: number;
+  ingredients: {
+    ingredient: string;
+    quantity?: number;
+    unit?: string | number;
+  }[];
+  ustensils: string[];
+
+  constructor(data: recipe) {
+    this.id = data.id;
+    this.name = data.name;
+    this.description = data.description;
+    this.time = data.time;
+    this.ingredients = data.ingredients;
+    this.ustensils = data.ustensils;
+  }
+
+  createCard(): HTMLElement {
+    const card = document.createElement("article");
+    const image = document.createElement("div");
+    const infos = document.createElement("div");
+    const header = document.createElement("header");
+    const ingredientList = document.createElement("ul");
+    const ingredientItem = document.createElement("li");
+    const description = document.createElement("p");
+
+    card.classList.add("recipe-card");
+    image.classList.add("recipe-img");
+    infos.classList.add("recipe-infos");
+
+    card.appendChild(image);
+    card.appendChild(infos);
+    infos.appendChild(header);
+    infos.appendChild(ingredientList);
+    infos.appendChild(description);
+
+    header.innerText = this.name
+
+    for (let ingredient of this.ingredients) {
+      ingredientList.appendChild(ingredientItem);
+      if (!ingredient.quantity && !ingredient.unit) {
+        ingredientItem.innerHTML = `<span class="ingredient-name">${ingredient.ingredient}</span>`;
+      } else if (!ingredient.unit) {
+        ingredientItem.innerHTML =
+          `<span class="ingredient-name">${ingredient.ingredient}: </span>` +
+          ingredient.quantity;
+      } else
+        ingredientItem.innerHTML =
+          `<span class="ingredient-name">${ingredient.ingredient}: </span>` +
+          ingredient.quantity +
+          " " +
+          ingredient.unit;
+    }
+
+    description.textContent = this.description;
+    return card;
+  }
+}
+
+export function createRecipeCard(recipes: any) {
+  const recipeSection = document.querySelector(
+    ".recipe-section"
+  ) as HTMLDivElement;
+  for (let recipe of recipes) {
+    recipeSection.appendChild(new Recipe(recipe).createCard());
+  }
+  return recipeSection;
+}
